d03c90867f512932eabc2f85b3f83c74bf7dad80
Surya Singh <115163053+suryasingh30@users.noreply.github.com>
2025-08-06 08:53:22 +0530

    Update urls.py- forgot to add "/"

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('api/products/', include('products.urls')),
-    path('api/cart', include('cart.urls')),
-    path('api/orders', include('orders.urls')),
-    path('api/wishlist', include('wishlist.urls'))
+    path('api/cart/', include('cart.urls')),
+    path('api/orders/', include('orders.urls')),
+    path('api/wishlist/', include('wishlist.urls'))
 ]
